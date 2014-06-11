7c9a401be603562b2389a30d089b31df7288c99a
Niko Junkala <baabelfish@gmail.com>
2014-06-12 01:16:09 +0300

    Added a better example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "mapper.hpp"
 
+struct Something {
+    int a;
+    int b;
+};
+
 struct Test {
     int x;
     int y;
     std::string z;
+    Something s;
 
     struct Wat {
         float a;
@@ -19,6 +25,7 @@ int main() {
         "x", initor::toInteger(&Test::x),
         "y", initor::toInteger(&Test::y),
         "z", initor::toString(&Test::z),
+        "s", [](Test& t, std::string v) { t.s = { std::stoi(v) * 2, std::stoi(v) / 2 }; },
         "nested", initor::Mapper<Test>::useMapper(&Test::nested, wat_parser)
     );
 
@@ -27,6 +34,8 @@ int main() {
     std::cout << "x:      " << n.x << std::endl;
     std::cout << "y:      " << n.y << std::endl;
     std::cout << "z:      " << n.z << std::endl;
+    std::cout << "s.a:    " << n.s.a << std::endl;
+    std::cout << "s.b:    " << n.s.b << std::endl;
     std::cout << "nested: " << n.nested.a << std::endl;
 
     return 0;
